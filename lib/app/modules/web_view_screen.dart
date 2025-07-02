950c79f498e1f7e32c7c849f98687d15062e2c63
fahamin <fahamin00@gmail.com>
2025-07-02 11:05:58 +0600

    web view loading add

--- lib/app/modules/web_view_screen.dart
+++ lib/app/modules/web_view_screen.dart
@@ -1,23 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 import '../../wigets/appbar_title.dart';
 
-class WebViewPage extends StatelessWidget {
+class WebViewPage extends StatefulWidget {
   final String url;
+
   const WebViewPage({required this.url});
+
+  @override
+  State<WebViewPage> createState() => _WebViewPageState();
+}
+
+class _WebViewPageState extends State<WebViewPage> {
+  var isLoading = true.obs;
+  late final WebViewController _controller;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    _controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setNavigationDelegate(NavigationDelegate(
+        onPageStarted: (String url) {
+          isLoading.value = true;
+        },
+        onPageFinished: (String url) {
+          isLoading.value = false;
+        },
+      ))
+      ..loadRequest(Uri.parse(widget.url));
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar:AppbarTitle("Article Details"),
-      body: WebViewWidget(
-        controller: WebViewController()
-      ..setJavaScriptMode(JavaScriptMode.unrestricted)
-      ..enableZoom(true)
-      ..getScrollPosition()
-      ..loadRequest(Uri.parse(url)),
-    ),
-    );
+        appBar: AppbarTitle("Article Details"),
+        body: Stack(
+            children: [
+              WebViewWidget(controller: _controller),
+              Obx(() => isLoading.value
+                  ? Center(child: CircularProgressIndicator())
+                  : SizedBox.shrink())
+            ],
+          ),
+        );
   }
 }
